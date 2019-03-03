f5c316a0ed7a03a4373eeed5470f585fb1de733a
OrShemesh1992 <chikos2011@gmail.com>
2019-03-03 09:38:42 +0200

    My Work

---
+++ Basiccheck.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+dirPath=$1
+program=$2
+file=./makefile
+if [ -e "$dirPath/$file" ]; then
+    make
+    if [ $? -eq 0 ]; then
+      echo "Compilation Pass"
+      valgrind --leak-check=full -v ./$program  > leaks.txt 2>&1
+      grep -q "no leaks are possible" leaks.txt
+      if [ $? -eq 0 ]; then
+        echo "Memory Leaks Pass"
+        rm leaks.txt
+        valgrind --tool=helgrind $dirPath/$program > threadRace.txt 2>&1
+        grep -q "ERROR SUMMARY: 0 errors" threadRace.txt
+        if [ $? -eq 0 ]; then
+          echo "Thread Race Pass"
+          rm threadRace.txt
+          exit 0
+        else
+          echo "Thread Race Fail"
+          rm threadRace.txt
+          exit 1
+        fi
+      else
+        echo "Memory Leaks Fail"
+        rm leaks.txt
+        exit 3
+      fi
+    else
+      echo "Compilation Fail"
+      exit 7
+    fi
+else
+    echo "Compilation Fail"
+    exit 7
+fi
