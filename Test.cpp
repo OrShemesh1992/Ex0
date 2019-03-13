6455a27f668c09bbf01b177567ffdc1ccdf49262
OrShemesh1992 <chikos2011@gmail.com>
2019-03-13 18:20:37 +0200

    My

--- Test.cpp
+++ Test.cpp
@@ -2,35 +2,36 @@
 #include <iostream>
 #include <thread>
 #include <pthread.h>
-struct Account{
-  int balance{100};
+struct Account {
+        int balance{100};
 };
-                                                      // 2
+// 2
 void transferMoney(int amount, Account& from, Account& to){
-  using namespace std::chrono_literals;
-  if (from.balance >= amount){
-    from.balance -= amount;
-    std::this_thread::sleep_for(1ns);                 // 3
-    to.balance += amount;
-  }
+        using namespace std::chrono_literals;
+        if (from.balance >= amount) {
+                from.balance -= amount;
+                std::this_thread::sleep_for(1ns);   // 3
+                to.balance += amount;
+        }
 }
 
 int main(){
-  int *stam = new int[333];
-  std::cout << std::endl;
+        int *stam = new int[333];
 
-  Account account1;
-  Account account2;
-                                                        // 1
-  std::thread thr1(transferMoney, 50, std::ref(account1), std::ref(account2));
-  std::thread thr2(transferMoney, 130, std::ref(account2), std::ref(account1));
+        std::cout << std::endl;
 
-  thr1.join();
-  thr2.join();
+        Account account1;
+        Account account2;
+        // 1
+        std::thread thr1(transferMoney, 50, std::ref(account1), std::ref(account2));
+        std::thread thr2(transferMoney, 130, std::ref(account2), std::ref(account1));
 
-  std::cout << "account1.balance: " << account1.balance << std::endl;
-  std::cout << "account2.balance: " << account2.balance << std::endl;
+        thr1.join();
+        thr2.join();
 
-  std::cout << std::endl;
+        std::cout << "account1.balance: " << account1.balance << std::endl;
+        std::cout << "account2.balance: " << account2.balance << std::endl;
+
+        std::cout << std::endl;
 
 }
